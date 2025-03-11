2d7602635ed208f09251173af77f6d3ecfbd2645
Pritesh Rajpurohit <priteshrajpurohit85@gmail.com>
2025-03-11 23:59:09 +0530

    No result feature updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ const App = () => {
   function removeTour(id){
     setTours(tours.filter(tour=>tour.id !== id))
   }
+  if(tours.length===0){
+    return (
+      <div className="refresh">
+        <h2> No tour are left</h2>
+        <button onClick={()=>setTours(data)}> Refresh</button>
+      </div>
+    )
+  }
   return( <div>
     <Tour tours={tours} removeTour={removeTour}></Tour>
   </div>
